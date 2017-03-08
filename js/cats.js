986f6030be5d461ec41892284ef7a1fb2eaad8ad
LizP <epritch@uw.edu>
2017-03-08 10:44:18 -0500

    uploaded in new repo

---
+++ js/cats.js
@@ -0,0 +1,41 @@
+var CATS = [
+    {
+        "location": "Seattle, WA",
+        "date": "September, 2015",
+        "description": "Airbnb cat",
+        "name": "Unknown, but he seemed like a Ralph",
+        "color": "Tabby - Grey",
+        "favoriteSong": "'The Final Countdown' by Europe"
+    },
+    {
+        "location": "Portland, OR",
+        "date": "September, 2015",
+        "description": "Neighborhood cat in St. Johns",
+        "name": "Definitely Not Rupert",
+        "color": "Tuxedo",
+        "favoriteSong": "'Bodies' by Drowning Pool"
+    },
+    {
+        "location": "Savannah, GA",
+        "date": "March, 2016",
+        "description": "Midtown mansions cat",
+        "name": "Something pretentious. Floyd.",
+        "color": "Grey and White",
+        "favoriteSong": "'Wait' by M83"
+    },
+    {
+        "location": "Savannah, GA",
+        "date": "March, 2016",
+        "description": "Community garden cat",
+        "name": "Probably Jacques",
+        "color": "Tuxedo",
+        "favoriteSong": "'Wolf' by First Aid Kit"
+    },
+    {
+        "location": "Seattle, WA",
+        "date": "June, 2016",
+        "description": "Erica Trotter's cat!",
+        "name": "Ranger. For real.",
+        "color": "Tabby - Orange",
+        "favoriteSong": ""
+    }]
